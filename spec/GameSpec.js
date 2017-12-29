1f2c92cc18b7a2cf118ae2ce0a63650f67e5df5e
majdeddine <majda-jambouri@hotmail.fr>
2017-12-29 17:09:54 +0000

    add next generation

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -25,12 +25,23 @@ describe("Game",function(){
     })
   })
 
-   // it("live cell with fewer than two live neighbours dies", function(){
-   //   expect(game.play([[1,1,0],[0,0,0],[0,0,0]])).toEqual([[0,0,0],[0,0,0],[0,0,0]])
-   // })
-   //
-   // it(" live cell with two or three live neighbours lives on to the next generation", function(){
-   //   expect(game.play([[1,1,1],[0,0,0],[0,0,0]])).toEqual([[0,1,0],[0,1,0],[0,0,0]])
-   // })
+  describe("nextGenration",function(){
+
+     it("live cell with fewer than two live neighbours dies", function(){
+       expect(game.play([[1,1,0],[0,0,0],[0,0,0]])).toEqual([[0,0,0],[0,0,0],[0,0,0]])
+     })
+
+     it(" live cell with two or three live neighbours lives on to the next generation", function(){
+       expect(game.play([[1,1,1],[0,0,0],[0,0,0]])).toEqual([[0,1,0],[0,1,0],[0,0,0]])
+     })
+
+     it("live cell with more than three live neighbours dies", function(){
+       expect(game.play([[1,0,1],[1,1,1],[1,0,1]])).toEqual([[1,0,1],[1,0,1],[1,0,1]])
+     })
+
+     it("dead cell with exactly three live neighbours becomes a live cell", function(){
+       expect(game.play([[0,0,0],[1,1,1],[0,0,0]])).toEqual([[0,1,0],[0,1,0],[0,1,0]])
+     })
+ });
 
 })
